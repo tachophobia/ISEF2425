2bdeef1856ef6a2555e1728bd0b8dbca54f60406
Nikita Lugovoy <niklugovoy@gmail.com>
2024-10-27 11:46:18 -0400

    restructured DBM.py

--- DBM.py
+++ DBM.py
@@ -7,21 +7,14 @@ class Cell:
         self.j = j
         self.phi = potential
 
-        self.phi_eta = None
-        self.prob = None
-
     def get_coords(self):
-        return self.i, self.j
-    
-    def get_neighbors(self):
-        return [(self.i+1, self.j), (self.i-1, self.j), (self.i, self.j+1), (self.i, self.j-1), (self.i+1, self.j+1), (self.i+1, self.j-1), (self.i-1, self.j+1), (self.i-1, self.j-1)]
-        
+        return self.i, self.j    
 
 class DielectricBreakdownModel:
     # based on "Fast Simulation of Laplacian Growth" by Kim et al.
     # fuse breakdown instead of dielectric breakdown simulated for effectively same result
 
-    def __init__(self, eta=1., dim=100, h=1, seed=None):
+    def __init__(self, eta=5., dim=100, h=1, seed=None):
         self.eta = eta
         # size of one grid block
         self.R1 = h/2
@@ -29,17 +22,25 @@ class DielectricBreakdownModel:
         # 0 = empty, 1 = filled
         self.dim = dim
         self.grid = np.zeros((2*dim+1, 2*dim+1))
-
-        self.pattern = []
-        self.environment = []
+        if not seed:
+            seed = [dim, dim]
+        
+        self.pattern = np.array([seed])
         self.candidates = []
+        self.environment = []
         self.hit_edge = False
+        self._generate_neighbors()
 
-        if seed:
-            seed = Cell(seed[0], seed[1], 1)
-        else:
-            seed = Cell(dim, dim, 1)
+        seed = Cell(seed[0], seed[1])
         self.add_cell(seed)
+
+    def _generate_neighbors(self):
+        self.neighbors = {}
+        for i in range(self.grid.shape[0]):
+            for j in range(self.grid.shape[1]):
+                self.neighbors[(i, j)] = [(i+1, j), (i-1, j), (i, j+1), (i, j-1),
+                                     (i+1, j+1), (i+1, j-1), (i-1, j+1), (i-1, j-1)]     
+                self.neighbors[(i, j)] = [n for n in self.neighbors[(i, j)] if 0 < n[0] < self.grid.shape[0] and 0 < n[1] < self.grid.shape[1]]
 
     def grow_pattern(self):
         cell = self.choose_candidate()
@@ -47,10 +48,10 @@ class DielectricBreakdownModel:
         self.update_potentials(cell)
 
     def choose_candidate(self):
-        phi_values = np.array([c.phi for c in self.candidates])
-        min_phi = np.min(phi_values)
+        phi_values = np.array([c.phi for c in self.candidates])  
         max_phi = np.max(phi_values)
-        
+        min_phi = np.min(phi_values)
+
         phi_eta = ((phi_values - min_phi) / (max_phi - min_phi)) ** self.eta
         phi_eta_sum = np.sum(phi_eta)
         probabilities = phi_eta / phi_eta_sum
@@ -58,42 +59,47 @@ class DielectricBreakdownModel:
         idx = np.random.choice(len(self.candidates), p=probabilities)
         return self.candidates.pop(idx)
 
-        
     def simulate(self, steps=None):
         if not steps:
             steps = 0
             while not self.hit_edge:
                 self.grow_pattern()
-                steps += 1   
+                steps += 1
         else:
             for _ in range(int(steps)):
                 self.grow_pattern()
         return steps
-        
-    def animate(self, steps: int):
-        frames = []
-        for _ in range(steps):
-            self.grow_pattern()
-            frames.append(self.grid.copy())
-        
-        return steps, frames
-            
+
     def add_cell(self, cell):
         self.grid[cell.i, cell.j] = 1
-        self.pattern.append(cell)
+        self.pattern = np.append(self.pattern, [[cell.i, cell.j]], axis=0)
 
-        possible_neighbors = cell.get_neighbors()
-        neighbors = [p for p in possible_neighbors if 0 < p[0] < self.grid.shape[0] and 0 < p[1] < self.grid.shape[1]]
-        if len(possible_neighbors) > len(neighbors):
+        neighbors = self.neighbors[(cell.i, cell.j)]
+        if len(neighbors) < 8:
             self.hit_edge = True
-        # only allow for neighbors in neutral space
-        neighbors = [p for p in neighbors if self.grid[p[0], p[1]] == 0]
-
-        for n in neighbors:
-            self.grid[n[0], n[1]] = -1
         
-        for p in neighbors:
-            self.candidates.append(Cell(p[0], p[1], self.calculate_spawn_potential(p[0], p[1])))
+        for (ni, nj) in neighbors:
+            # only allow for neighbors in neutral space
+            if self.grid[ni, nj] != 0:
+                continue
+            self.grid[ni, nj] = -1
+            self.candidates.append(Cell(ni, nj, self.calculate_spawn_potential(ni, nj)))
+        
+    def calculate_spawn_potential(self, i, j):
+        # equation 10
+        pattern_distances = np.sqrt((i - self.pattern[:, 0]) ** 2 + (j - self.pattern[:, 1]) ** 2)
+        neighbor_potential = np.sum(1 - self.R1 / pattern_distances)
+
+        environment_potential = np.sum([c.phi / np.sqrt((i - c.i) ** 2 + (j - c.j) ** 2) for c in self.environment])
+        return neighbor_potential + environment_potential
+
+    def update_potentials(self, cell):
+        # equation 11
+        candidate_coords = np.array([[c.i, c.j] for c in self.candidates])
+        distances = np.sqrt((candidate_coords[:, 0] - cell.i) ** 2 + (candidate_coords[:, 1] - cell.j) ** 2)
+        delta_potentials = 1 - self.R1 / distances
+        for c, delta in zip(self.candidates, delta_potentials):
+            c.phi += delta
 
     def add_line_of_charge(self, p1, p2, phi=-1):
         # create a line starting from p1 to p2
@@ -147,31 +153,13 @@ class DielectricBreakdownModel:
                     cell = Cell(x, y, phi)
                     self.grid[x, y] = phi
                     self.environment.append(cell)
-        
-    def calculate_spawn_potential(self, i, j):
-        # equation 10
-        pattern_coords = np.array([(c.i, c.j) for c in self.pattern])
-        pattern_distances = np.sqrt((i - pattern_coords[:, 0]) ** 2 + (j - pattern_coords[:, 1]) ** 2)
-        neighbor_potential = np.sum(1 - self.R1 / pattern_distances)
-
-        environment_potential = np.sum([c.phi / np.sqrt((i - c.i) ** 2 + (j - c.j) ** 2) for c in self.environment])
-        return neighbor_potential + environment_potential
-
-    
-    def update_potentials(self, cell):
-        # equation 11
-        candidate_coords = np.array([(c.i, c.j) for c in self.candidates])
-        distances = np.sqrt((candidate_coords[:, 0] - cell.i) ** 2 + (candidate_coords[:, 1] - cell.j) ** 2)
-        delta_potentials = 1 - self.R1 / distances
-        for c, delta in zip(self.candidates, delta_potentials):
-            c.phi += delta
-
 
     def show(self, show_environment=False):
         img = self.grid
         if not show_environment:
             img[np.where(img!=1)] = 0
-            plt.imshow(img, cmap='afmhot')
+            img = 1 - img
+            plt.imshow(img, cmap='gray')
         else:
             img[np.where(img > 1)] = 2
             img[np.where(img < -1)] = -2
@@ -184,3 +172,9 @@ class DielectricBreakdownModel:
     
     def load(self, path: str):
         self.grid = np.load(path)
+
+
+if __name__ == "__main__":
+    model = DielectricBreakdownModel(eta=5, dim=300)
+    model.simulate()
+    model.show()
