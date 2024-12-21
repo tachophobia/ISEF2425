ad64bc79531c8ebcb11315338fa05b1b6cc76ec2
Ethan O'Hara <Ethan.i.ohara@gmail.com>
2024-12-21 17:14:54 -0500

    Updated ContinuousSubmarine to include Newton's 2nd Law calculations and to align with gym.Env

--- Environments.py
+++ Environments.py
@@ -168,44 +168,57 @@ class Submarine:
     
 
 
-class ContinuousSubmarine:
+class ContinuousSubmarine(gym.Env): 
     def __init__(self,width,height,start_pos,tWidth,tHeight,max_y,action_space):
         self.w=width #width of environment
         self.h=height #height of environment
         self.tW=tWidth #width of terminal areas
         self.tH=tHeight #height of terminal areas
-        self.pos=start_pos #always in (y,x) form, this time it's floats rather than ints
+        self.state=[start_pos,0,0] #always in [(y,x),Vy,Vx] form, this time it's floats rather than ints
         self.max_y=max_y #dictionary describing the height of the "seafloor" in form {xPos:maxY} -- xPos and maxY are both ints
         self.action_space=action_space
+        self.time_steps=0
 
     def set_rewards(self,rewards):
         self.rewards = rewards
 
     def set_state(self, s):
-        self.pos=s  
+        self.state=s  
 
     def get_state(self):
         """Return the state the agent is in as a tuple representing position."""
-        return self.pos
+        return self.state
 
     def is_terminal(self,s): #given pos s, return if it's terminal
         return to_box(s) in self.rewards
 
     def get_next_state(self, s, a): #s-->current state; a-->action in form (theta,magnitude) tuple; a vector representing the action; a will always be valid given s and the environment
         next_pos=s
-        delta_X=np.cos(a[0])*a[1]
-        delta_Y=np.sin(a[0])*a[1]
-        next_pos[0]=s[0]+delta_Y
-        next_pos[1]=s[1]+delta_X
+        delta_X,delta_Y,vX,vY = CAKE(a)
+        next_pos[0][0]=s[0][0]+delta_Y
+        next_pos[0][1]=s[0][1]+delta_X
+        next_pos[1]=vY
+        next_pos[2]=vX
         return next_pos
     
-    def action(self,a): #a-->action in form (theta,magnitude) tuple; a vector representing the action; action will always be valid given s and the environment
-        """Update the position of the agent based on action a and return the reward associated with the new position."""
-        delta_X=np.cos(a[0])*a[1]
-        delta_Y=np.sin(a[0])*a[1]
-        self.pos[0]+=delta_Y
-        self.pos[1]+=delta_X
-        return self.rewards.get(to_box(self.pos),a[1])  #the agent loses reward scaling with the size of the movement
+    def step(a): #a-->action in form (theta,magnitude) tuple; a vector representing the action; action will always be valid given s and the environment; magnitude is magnitude of the force
+        if is_valid_action(a): 
+            self.last_pos = self.state
+            delta_X,delta_Y,vX,vY = CAKE(a)
+            self.state[0][0]+=delta_Y
+            self.state[0][1]+=delta_X
+            self.state[1]=vY
+            self.state[2]=vX
+            self.time_steps+=1
+            return (self.state, self.rewards.get(to_box(self.state[0]),-1*a[1]), is_terminal(self.state[0]),self.time_steps>1000,{})  #the agent loses reward scaling with the size of the movement; the return is in line with Env.step's API
+        delta_X,delta_Y,vX,vY = CAKE(a)
+        self.state[0][0]+=delta_Y
+        self.state[0][1]+=delta_X
+        self.state[1]=vY
+        self.state[2]=vX
+        self.time_steps+=1
+        return (self.state,0,True,self.time_steps>1000,{})  #the return if action isn't valid
+
 
     def to_box(pos): #takes pos and converts it to a coordinate representing the larger box
         y,x=int(pos[0]),int(pos[1])
@@ -213,11 +226,8 @@ class ContinuousSubmarine:
 
     def undo_action(self, a):
         """Update the position of the agent by doing the reverse of action a."""
-        delta_X=np.cos(a[0])*a[1]
-        delta_Y=np.sin(a[0])*a[1]
-        self.pos[0]-=delta_Y
-        self.pos[1]-=delta_X
-        assert(self.get_State()[0]>=0 and self.get_State()[0]<=self.h and self.get_State()[1]>=0 and self.get_State()[1<=w])
+        self.state=self.last_pos
+        assert(self.get_State()[0][0]>=0 and self.get_State()[0][0]<=self.h and self.get_State()[0][1]>=0 and self.get_State()[0][1]<=self.w)
 
     def get_terminal_states(self):  #returns the top-left corners of each terminal box; combined with tW and tH, this can be used to determine if a pos is terminal
         return [key for key in self.rewards]
@@ -225,11 +235,21 @@ class ContinuousSubmarine:
     def is_valid_action(self,a):
         b= a[0]<2*np.pi and a[0]>0 and a[1]<1.5 and a[1]>.5
         if not b:return False
-        next_state = get_next_state(self,self.pos,a)
+        next_state = get_next_state(self,self.state,a)
         if next_state[1]>self.w or next_state[1]<0 or next_state[0]<0: return False
         next_state_to_box=to_box(next_state)
         if next_state[0]>=self.max_y[next_state_to_box[1]]:return False
         return True
+
+    def CAKE(self,a):
+        x_accel=a[1]*np.cos(a[0])
+        y_accel = a[1]*np.sin(a[0])
+        delta_t=2
+        delta_x = self.state[2] * delta_t + (x_accel * (delta_t^2))
+        delta_y=self.state[1]*delta_t + y_accel*(delta_t^2)
+        vX=self.state[2] +delta_t*x_accel
+        vY=self.state[1] +delta_t*y_accel
+        return delta_x,delta_y,vX,vY
 
 
     def standard_env():
@@ -237,12 +257,10 @@ class ContinuousSubmarine:
         tH=5
         w=50
         h=55
-        start_pos=(0,0)
+        state=[(0,0),0,0]
         rewards={(5,0):1,(10,5):2,(15,10):3,(20,15):5,(20,20):8,(20,25):16,(35,30):24,(35,35):50,(45,40):74,(50,45):124}
         max_y={0:10,5:15,10:20,15:25,20:25,25:25,30:40,35:40,40:50,45:55} 
-        action_space=(gym.spaces.Box(0,2*(np.pi)),gym.spaces.Box(.5,1.5))
-        
-        
+        action_space=(gym.spaces.Box(0,2*(np.pi)),gym.spaces.Box(0.05,5.5))
 
         grid = ["."]*(w*h)
         for x in range(w):
@@ -258,7 +276,7 @@ class ContinuousSubmarine:
         pass
     
     
-    standardEnv()
+    standard_env()
 
 
 
