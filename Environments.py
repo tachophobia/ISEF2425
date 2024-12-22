6c01b3a8f69d6ef912414f3d29da3c891fe32476
Nikita Lugovoy <niklugovoy@gmail.com>
2024-12-22 11:34:53 -0500

    revise to extend gym.Env

--- Environments.py
+++ Environments.py
@@ -1,282 +1,138 @@
 import numpy as np
-import random
-import matplotlib.pyplot as plt
 import gym
-from abc import ABC,abstractmethod
+from gym import spaces
 
-class Environment(ABC):
-    @abstractmethod
-    def set_rewards(self, rewards, actions):
-        """Set rewards and actions for the environment."""
-        pass
-
-    @abstractmethod
-    def set_state(self,s):
-        """Set to a new state s (often the position of the agent)."""
-        pass
-
-    @abstractmethod
-    def get_state(self):
-        """Return the current state."""
-        pass
-
-    @abstractmethod
-    def is_terminal(self,s):
-        """Evaluate whether state s is terminal."""
-        pass
-
-    @abstractmethod
-    def get_next_state(self,s,a):
-        """Return next state given current state s and action a."""
-        pass
-
-    @abstractmethod
-    def action(self,a):
-        """Execute action a."""
-        pass
-
-    @abstractmethod
-    def undo_action(self,a):
-        """Undo action a."""
-        pass
-
-    @abstractmethod
-    def all_states(self):
-        """Returns the set of all states and corresponding rewards."""
-        pass
-
-class Submarine:
-    def __init__(self,rows,cols,startPos):
-        """Initialize the size of the environment and the starting position of the agent.
+class ContinuousSubmarine(gym.Env):
+    def __init__(self, **kwargs):
+        super().__init__()
         
-        Args:
-            rows (int): the height of the environment.
-            cols (int): the width of the environment.
-            startPos (tuple): the starting position of the agent.
-        """
-        self.rows=rows
-        self.cols=cols
-        self.pos=startPos
-        self.actions={'U','R','D','L'}
-    def set(self,rewards,actions):
-        self.rewards,self.actions = rewards,actions
-    def setState(self, s):
-        self.pos=s  
-    def getState(self):
-        """Return the state the agent is in as a tuple representing position."""
-        return self.pos
-    def isTerminal(self,s):
-        """Return a bool of whether the state is terminal, based on whether there are any possible actions in the state s."""
-        return s not in self.actions
-    def getNextState(self, s, a):
-        """
-        Return the next state.
+        # Set default parameters and update with kwargs
+        defaults = {
+            'width': 50,
+            'height': 55,
+            'start_pos': (0, 0),
+            'terminal_width': 5,
+            'terminal_height': 5,
+            'max_y': {0: 10, 5: 15, 10: 20, 15: 25, 20: 25, 25: 25, 30: 40, 35: 40, 40: 50, 45: 55},
+            'rewards': {(5, 0): 1, (10, 5): 2, (15, 10): 3, (20, 15): 5, (20, 20): 8, (20, 25): 16, (35, 30): 24, (35, 35): 50, (45, 40): 74, (50, 45): 124},
+        }
+        params = {**defaults, **kwargs}
+
+        # Environment dimensions and terminal areas
+        self.width = params['width']
+        self.height = params['height']
+        self.terminal_width = params['terminal_width']
+        self.terminal_height = params['terminal_height']
+
+        # Initial state: [y, x, vy, vx]
+        self.state = [*params['start_pos'], 0, 0]
+
+        # Seafloor boundary represented as {x: max_y}
+        self.max_y = params['max_y']
+
+        # Rewards for terminal areas
+        self.rewards = params['rewards']
+
+        # Action space: [theta (0 to 2*pi), magnitude (0.05 to 1.5)]
+        self.action_space = spaces.Box(
+            low=np.array([0, 0.05]),
+            high=np.array([2 * np.pi, 1.5]),
+            dtype=np.float64
+        )
+
+        # Observation space: [y, x, vy, vx] bounded by environment dimensions and reasonable velocities
+        self.observation_space = spaces.Box(
+            low=np.array([0, 0, -np.inf, -np.inf]),
+            high=np.array([self.height, self.width, np.inf, np.inf]),
+            dtype=np.float64
+        )
+
+        # Time steps counter
+        self.time_steps = 0
+
+    def reset(self):
+        """Reset the environment to the initial state."""
+        self.state = [0, 0, 0, 0]
+        self.time_steps = 0
+        return np.array(self.state, dtype=np.float32)
+
+    def step(self, action):
+        """Perform one step in the environment based on the given action."""
+        assert self.action_space.contains(action), "Invalid action."
         
-        Args:
-            s (tuple): the initial state.
-            a (string): the action taken.
-            
-        Returns:
-            tuple: the final state.
-        """
-        i,j=s[0],s[1]
-        if a in self.actions[s]:
-            if a=='U': i-=1
-            elif a=='D': i+=1
-            elif a=='R': j+=1
-            elif a=='L': j-=1
-        return (i,j)
-    def action(self,a):
-        """Update the position of the agent based on action a and return the reward associated with the new position."""
-        if a in self.actions[self.pos]:
-            if a=='U': self.pos[0]-=1
-            elif a=='D': self.pos[0]+=1
-            elif a=='R': self.pos[1]+=1
-            elif a=='L': self.pos[1]-=1
-        return self.rewards.get(self.pos,0)
-    def undoAction(self, a):
-        """Update the position of the agent by doing the reverse of action a."""
-        if a in self.actions[self.pos]:
-            if a=='U': self.pos[0]+=1
-            elif a=='D': self.pos[0]-=1
-            elif a=='R': self.pos[1]-=1
-            elif a=='L': self.pos[1]+=1
-        assert(self.getState() in self.allStates())
-    def allStates(self):
-        """Return the set of all possible states, including terminal ones."""
-        return set(self.actions.keys()) | set(self.rewards.keys())
-    def generateRandom(self, rowNum,colNum,chanceToDecrease):
-        """
-        Generate a random environment of specified size.
+        delta_x, delta_y, vx, vy = self._calculate_dynamics(action)
+        self.state[0] += delta_y
+        self.state[1] += delta_x
+        self.state[2] = vy
+        self.state[3] = vx
+        self.time_steps += 1
         
-        Args:
-            rowNum (int): the height of the environment.
-            colNum (int): the width of the environment.
-            chanceToDecrease (float): the probability (1-100) of going down a level when assigning rewards.
-            
-        Returns:
-            dict {tuple: int}: the value of the reward given for being in each state.
-            dict {tuple: set}: the set of possible actions in each state.
-        """
-        rewards = {(0,1):1}  #all indexed column, row (i,j)
-        actions = {}
-        topOfCol={0:1}
-        j=1
-        r = [1,2,3,5,8,16,24,50,74,124]
-        for i in range(1,colNum): #setting terminal states
-            toss = random.randint(1,100)
-            if toss<=chanceToDecrease and (j+1)<rowNum:
-                j+=1
-            topOfCol[i]=j
-            rewards[(i,j)]=r[i]
-        for i in range(0,colNum): #setting state-action dict and negative time rewards
-            j=0
-            while (i,j) not in rewards:
-                rewards[(i,j)]=-1
-                ac= set()
-                if i-1>0 and j<=topOfCol[i-1]: ac.add('L')
-                if i+1<colNum and j<=topOfCol[i+1]: ac.add('R')
-                if j+1<=topOfCol[i]: ac.add('D')
-                if j-1>=0 and j-1<=topOfCol[i]: ac.add('U')
-                actions[(i,j)]=ac
-                j+=1
-        return actions,rewards
+        # Clip state to environment bounds
+        self.state[0] = max(0, min(self.state[0], self.height))
+        self.state[1] = max(0, min(self.state[1], self.width))
+        
+        # Determine if the episode is terminal
+        terminal = self._is_terminal()
 
-    def standardSubmarine(self):
-        rows=11
-        cols=10
-        actions,rewards = self.generateRandom(11,10,80)
-        for j in range(rows):
-                for i in range(cols):
-                    if (i,j) in rewards: 
-                        st = f"{rewards[i,j]}"
-                        a=len(st)
-                        print(f"{rewards[i,j]}{' '*(3-a)}",end="")
-                    else:print("X  ",end="")
-                print()
-    
-    def printSub(self):
-        """Print the environment grid with associated rewards in each position, or an X for impossible states."""
-        for j in range(self.rows):
-            for i in range(self.cols):
-                if (i,j) in self.rewards: 
-                    st = f"{self.rewards[i,j]}"
-                    a=len(st)
-                    print(f"{self.rewards[i,j]}{' '*(3-a)}",end="")
-                else:print("X  ",end="")
-            print()
-    
+        # Compute reward
+        reward = self.rewards.get(self._to_box(self.state[:2]), -action[1])
+        
+        # Gym API requires returning (observation, reward, done, info)
+        done = terminal or self.time_steps > 1000
+        return np.array(self.state, dtype=np.float32), reward, done, {}
 
+    def render(self, mode="human"):
+        """Render the environment."""
+        grid = ["."] * (self.width * self.height)
+        for x in range(self.width):
+            for y in range(self.height):
+                if self._to_box([y, x]) in self.rewards:
+                    grid[x + y * self.width] = "T"
+                elif y >= self.max_y.get((x // self.terminal_width) * self.terminal_width, self.height):
+                    grid[x + y * self.width] = "X"
+        print("\n".join(["".join(grid[i:i + self.width]) for i in range(0, self.width * self.height, self.width)]))
 
-class ContinuousSubmarine(gym.Env): 
-    def __init__(self,width,height,start_pos,tWidth,tHeight,max_y,action_space):
-        self.w=width #width of environment
-        self.h=height #height of environment
-        self.tW=tWidth #width of terminal areas
-        self.tH=tHeight #height of terminal areas
-        self.state=[start_pos,0,0] #always in [(y,x),Vy,Vx] form, this time it's floats rather than ints
-        self.max_y=max_y #dictionary describing the height of the "seafloor" in form {xPos:maxY} -- xPos and maxY are both ints
-        self.action_space=action_space
-        self.time_steps=0
-
-    def set_rewards(self,rewards):
-        self.rewards = rewards
-
-    def set_state(self, s):
-        self.state=s  
-
-    def get_state(self):
-        """Return the state the agent is in as a tuple representing position."""
-        return self.state
-
-    def is_terminal(self,s): #given pos s, return if it's terminal
-        return self.to_box(s) in self.rewards
-
-    def get_next_state(self, s, a): #s-->current state; a-->action in form (theta,magnitude) tuple; a vector representing the action; a will always be valid given s and the environment
-        next_pos=s
-        delta_X,delta_Y,vX,vY = self.CAKE(a)
-        next_pos[0][0]=s[0][0]+delta_Y
-        next_pos[0][1]=s[0][1]+delta_X
-        next_pos[1]=vY
-        next_pos[2]=vX
-        return next_pos
-    
-    def step(self, a): #a-->action in form (theta,magnitude) tuple; a vector representing the action; action will always be valid given s and the environment; magnitude is magnitude of the force
-        if self.is_valid_action(a): 
-            self.last_pos = self.state
-            delta_X,delta_Y,vX,vY = self.CAKE(a)
-            self.state[0][0]+=delta_Y
-            self.state[0][1]+=delta_X
-            self.state[1]=vY
-            self.state[2]=vX
-            self.time_steps+=1
-            return (self.state, self.rewards.get(self.to_box(self.state[0]),-1*a[1]), self.is_terminal(self.state[0]),self.time_steps>1000,{})  #the agent loses reward scaling with the size of the movement; the return is in line with Env.step's API
-        delta_X,delta_Y,vX,vY = self.CAKE(a)
-        self.state[0][0]+=delta_Y
-        self.state[0][1]+=delta_X
-        self.state[1]=vY
-        self.state[2]=vX
-        self.time_steps+=1
-        return (self.state,0,True,self.time_steps>1000,{})  #the return if action isn't valid
-
-
-    def to_box(pos): #takes pos and converts it to a coordinate representing the larger box
-        y,x=int(pos[0]),int(pos[1])
-        return ((y//5)*5,(x//5)*5)
-
-    def undo_action(self, a):
-        """Update the position of the agent by doing the reverse of action a."""
-        self.state=self.last_pos
-        assert(self.get_State()[0][0]>=0 and self.get_State()[0][0]<=self.h and self.get_State()[0][1]>=0 and self.get_State()[0][1]<=self.w)
-
-    def get_terminal_states(self):  #returns the top-left corners of each terminal box; combined with tW and tH, this can be used to determine if a pos is terminal
-        return [key for key in self.rewards]
-
-    def is_valid_action(self,a):
-        b= a[0]<2*np.pi and a[0]>0 and a[1]<1.5 and a[1]>.5
-        if not b:return False
-        next_state = self.get_next_state(self,self.state,a)
-        if next_state[1]>self.w or next_state[1]<0 or next_state[0]<0: return False
-        next_state_to_box=self.to_box(next_state)
-        if next_state[0]>=self.max_y[next_state_to_box[1]]:return False
-        return True
-
-    def CAKE(self,a):
-        x_accel=a[1]*np.cos(a[0])
-        y_accel = a[1]*np.sin(a[0])
-        delta_t=2
-        delta_x = self.state[2] * delta_t + (x_accel * (delta_t^2))
-        delta_y=self.state[1]*delta_t + y_accel*(delta_t^2)
-        vX=self.state[2] +delta_t*x_accel
-        vY=self.state[1] +delta_t*y_accel
-        return delta_x,delta_y,vX,vY
-
-
-    def standard_env():
-        tW=5
-        tH=5
-        w=50
-        h=55
-        state=[(0,0),0,0]
-        rewards={(5,0):1,(10,5):2,(15,10):3,(20,15):5,(20,20):8,(20,25):16,(35,30):24,(35,35):50,(45,40):74,(50,45):124}
-        max_y={0:10,5:15,10:20,15:25,20:25,25:25,30:40,35:40,40:50,45:55} 
-        action_space=(gym.spaces.Box(0,2*(np.pi)),gym.spaces.Box(0.05,5.5))
-
-        grid = ["."]*(w*h)
-        for x in range(w):
-            for y in range(h):
-                if ((y//5)*5,(x//5)*5) in rewards:
-                    grid[x+y*w]="t"
-                elif y>=max_y[(x//5)*5]:
-                    grid[x+y*w]="X"
-        print("\n".join(["".join(grid[i:i+w]) for i in range(0,w*h,w)]))
-
-    
-    def print_grid():
+    def close(self):
+        """Cleanup the environment (if needed)."""
         pass
 
+    def _is_terminal(self):
+        """Check if the current state is terminal."""
+        return tuple(self._to_box(self.state[:2])) in self.rewards
 
+    def _to_box(self, pos):
+        """Convert position to the top-left corner of the larger box."""
+        y, x = int(pos[0]), int(pos[1])
+        return (y // self.terminal_height) * self.terminal_height, (x // self.terminal_width) * self.terminal_width
 
+    def _calculate_dynamics(self, action):
+        """Calculate the new state dynamics given an action."""
+        theta, magnitude = action
+        x_accel = magnitude * np.cos(theta)
+        y_accel = magnitude * np.sin(theta)
+        delta_t = 2
+        delta_x = self.state[3] * delta_t + 0.5 * x_accel * (delta_t ** 2)
+        delta_y = self.state[2] * delta_t + 0.5 * y_accel * (delta_t ** 2)
+        vx = self.state[3] + delta_t * x_accel
+        vy = self.state[2] + delta_t * y_accel
+        return delta_x, delta_y, vx, vy
 
-    
-    
+    def is_valid_action(self, action):
+        """Check if the given action is valid in the current state."""
+        theta, magnitude = action
+        if not (0 <= theta <= 2 * np.pi and 0.05 <= magnitude <= 1.5):
+            return False
+        
+        delta_x, delta_y, _, _ = self._calculate_dynamics(action)
+        next_x = self.state[1] + delta_x
+        next_y = self.state[0] + delta_y
+        if next_x < 0 or next_x > self.width or next_y < 0:
+            return False
+        next_box = self._to_box([next_y, next_x])
+        return next_y <= self.max_y.get(next_box[1], self.height)
+
+# Example usage
+if __name__ == "__main__":
+    env = ContinuousSubmarine()
+    env.reset()
+    env.render()
